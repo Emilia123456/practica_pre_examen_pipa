8fa7727f94376c8e0d1e6ef64acbd3e26212d043
MILENA AVRIL DAWIDOWICZ <47807116@ort>
2023-09-18 08:11:32 -0300

    hola

---
+++ Models/BD.cs
@@ -0,0 +1,10 @@
+using System.Data.SqlClient;
+using Dapper;
+
+public class BD{
+
+    private static string _connectionString = @"Server=localhost; DataBase=GestionAlumnos;Trusted_Connection=True;";
+
+
+}
+
